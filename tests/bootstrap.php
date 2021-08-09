56a283f3a556fe5e8d584e37f697e7ee0033408f
Wilkins <thibault@taillandier.name>
2021-08-09 23:39:00 +0200

    Unit Testing - Improve composer testing autoload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,6 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/phpunit/unit/NamespaceProviderEn.php';
-require_once __DIR__.'/phpunit/unit/NamespaceProviderFr.php';
-require_once __DIR__.'/phpunit/unit/NamespaceManagerBase.php';
-
-
-
-# echo "BOOTSTRAP\n";
 
 error_reporting( E_ALL );
 ini_set( 'display_errors', 1 );
